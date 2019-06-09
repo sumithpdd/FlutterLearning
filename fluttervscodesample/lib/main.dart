b3d820222ead214b59def87b46256478440fa593
sumith damodaran <sumithpd@gmail.com>
2019-06-10 00:56:14 +0200

    Working version , vs code on mac

---
+++ fluttervscodesample/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Flutter VS Code Sample",
+    theme: ThemeData(
+      primarySwatch: Colors.red,
+    ),
+    home: Text("Welcome to Flutter with VS Code"),
+  ));
+}
