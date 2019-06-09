87051195d93acb905943b21b2e5792fbe845eb47
sumith damodaran <sumithpd@gmail.com>
2019-06-10 01:23:32 +0200

    added text scale factor and text align

--- fluttervscodesample/lib/main.dart
+++ fluttervscodesample/lib/main.dart
@@ -1,25 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Flutter VS Code Sample",
-    theme: ThemeData(
-      primarySwatch: Colors.red,
-    ),
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("First flutter App using vs code."),
+  runApp(
+    MaterialApp(
+      title: "Flutter VS Code Sample",
+      theme: ThemeData(
+        primarySwatch: Colors.red,
       ),
-      body: Text("Welcome to my flutter first app"),
+      home: HomePage(),
     ),
-  ));
+  );
 }
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("First flutter App using vs code."),
+      ),
+      body: Center(child: Text("Welcome to my flutter first app", 
+      textAlign: TextAlign.center,
+     textScaleFactor: 2, style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),)),
     );
   }
 }
